b70e34f2c98e3dae9a2a1faad13157889fbe0e6e
iankang <ianngech@gmail.com>
2021-06-07 23:37:57 +0300

    binary search

---
+++ src/1.BinarySearching.kt
@@ -0,0 +1,13 @@
+ fun main(args:Array<String>){
+     println("oya")
+ }
+
+ fun binarySearching(list:List<Int>, item:Int){
+     val low = 0
+     var high = list.size.minus(1)
+
+     while (low<=high){
+
+     }
+
+ }
