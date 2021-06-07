052e58a323b50d965bddc494dbb6002a99e2e478
iankang <ianngech@gmail.com>
2021-06-07 23:58:57 +0300

    binary search implemented

--- src/1.BinarySearching.kt
+++ src/1.BinarySearching.kt
@@ -1,13 +1,24 @@
- fun main(args:Array<String>){
-     println("oya")
- }
+import kotlin.math.roundToInt
 
- fun binarySearching(list:List<Int>, item:Int){
-     val low = 0
-     var high = list.size.minus(1)
+fun main(args: Array<String>) {
+    var numbers = listOf(1,3,5,7,9)
+    println(binarySearching(numbers,3))
+    println(binarySearching(numbers, -1))
+}
 
-     while (low<=high){
+fun binarySearching(list: List<Int>, item: Int): Int? {
+    var low = 0
+    var high = list.size.minus(1)
 
-     }
+    while (low <= high) {
+        var mid = low.plus(high).div(2.0).roundToInt()
+        var guess = list[mid]
 
- }
+        when (true) {
+            guess == item -> return mid
+            guess > item -> high = mid - 1
+            else -> low = mid + 1
+        }
+    }
+    return null
+}
